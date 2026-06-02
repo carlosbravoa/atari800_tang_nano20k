34b0e49b649a6d256f125ada5b8c44e7b7919700
Carlos Bravo <cbravoa@gmail.com>
2026-06-02 13:14:19 -0400

    updating flash script

--- flash_it.sh
+++ flash_it.sh
@@ -3,8 +3,9 @@
 CYAN='\033[36m'
 NC='\033[0m'
 
-echo -e "${CYAN}Flashing firmware from $(date -d "$(stat -c '%y' firmware/firmware.bin)" '+%B %d, %Y')${NC}"
-sudo openFPGALoader -b tangnano20k -f -o 0x500000 firmware/firmware.bin
+# firmware is no longer separate
+#echo -e "${CYAN}Flashing firmware from $(date -d "$(stat -c '%y' firmware/firmware.bin)" '+%B %d, %Y')${NC}"
+#sudo openFPGALoader -b tangnano20k -f -o 0x500000 firmware/firmware.bin
 
 echo -e "${CYAN}Flashing Bitstream from $(date -d "$(stat -c '%y' impl/atari800_tn20k/impl/pnr/atari800_tn20k.fs)" '+%B %d, %Y')${NC}"
 sudo openFPGALoader -b tangnano20k -f impl/atari800_tn20k/impl/pnr/atari800_tn20k.fs
